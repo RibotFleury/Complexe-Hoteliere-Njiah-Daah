7047f102b4da2921549fa27f806e5f3b56f4882c
Ribot Fleury <teneceskoutse@gmail.com>
2025-01-07 14:50:06 -0500

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 # db/seeds.rb
 
 # Supprime les anciens services
+Reservation.destroy_all
 Service.destroy_all
 
 # Crée des nouveaux services
 services = [
-  { name: "chambre simple", description: "Une chambre simple avec lit confortable, TV et connexion Wi-Fi." },
-  { name: "chambre climatisée", description: "Une chambre avec climatisation, lit confortable, TV et connexion Wi-Fi." },
-  { name: "appartement meublé", description: "Un appartement entièrement meublé avec cuisine équipée, séjour, et chambre séparée." }
+  { name: "Chambres haut standing", description: "Des chambres élégantes et modernes, offrant tout le confort nécessaire, y compris un lit douillet, une douche moderne et un petit déjeuner continental inclus." },
+  { name: "Studios meublés", description: "Des studios entièrement équipés avec cuisine moderne, salon à manger pour quatre, lit confortable et un petit déjeuner continental inclus." },
+  { name: "Appartements meublés", description: "Des appartements spacieux avec deux chambres, une cuisine moderne, un séjour confortable et un petit déjeuner continental inclus." }
 ]
 
 services.each do |service|
